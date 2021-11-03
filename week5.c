d6fff39f9a09022d40aa8529cc0ee21a4e783032
KimTaering <wanghope@dgu.ac.kr>
2021-11-03 23:23:06 +0900

    week 5

---
+++ week5.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	// 반복문
+	// 핵심 : 명확한 조건에 따라 종료되어야 함!!
+	// for, while, do~while
+	/*
+	while (조건식)
+	{
+		(실행문);
+	}
+
+	for (초기식; 조건식; 증감식)
+	{
+		(실행문);
+	}
+	
+	do {
+		실행문;
+	} while(조건식);
+	*/
+
+	return 0;
+}
